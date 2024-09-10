2513214c35282015bc10eac0b93072169b69b92b
Margarita P <rprivalko@gmail.com>
2024-09-10 22:42:22 +0300

    main add: icons

---
+++ src/shared/icons/logo.tsx
@@ -0,0 +1,34 @@
+import {SVGProps} from "react";
+
+export const LogoIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg width="31" height="32" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <rect x="1.52173" y="2.30408" width="26.8897" height="26.8897" rx="13.4448" fill="#3177F2"/>
+        <path d="M14.9666 29.1938C22.392 29.1938 28.4114 23.1743 28.4114 15.7489C28.4114 8.32354 22.392 2.30408 14.9666 2.30408C7.54119 2.30408 1.52173 8.32354 1.52173 15.7489C1.52173 23.1743 7.54119 29.1938 14.9666 29.1938Z" stroke="white" stroke-width="1.36583" stroke-miterlimit="10"/>
+        <path d="M28.1485 15.2615C22.3914 9.83817 14.235 8.50522 7.30121 11.165C7.0493 11.2626 6.80046 11.3633 6.55469 11.47" stroke="url(#paint0_linear_31_1738)" stroke-width="1.34448" stroke-miterlimit="10" stroke-linecap="round"/>
+        <path d="M24.6001 18.635C16.8461 22.1366 7.64515 20.2947 1.54395 15.1887" stroke="url(#paint1_linear_31_1738)" stroke-width="1.34448" stroke-miterlimit="10" stroke-linecap="round"/>
+        <path d="M15.1235 29.1938C20.2051 22.8243 21.4307 13.8447 18.0444 7.01239C17.9236 6.76586 17.7936 6.52013 17.6608 6.27772" stroke="url(#paint2_linear_31_1738)" stroke-width="1.34448" stroke-miterlimit="10" stroke-linecap="round"/>
+        <path d="M12.316 24.3504C8.24272 17.2176 8.92769 8.70184 13.3686 2.41048" stroke="url(#paint3_linear_31_1738)" stroke-width="1.34448" stroke-miterlimit="10" stroke-linecap="round"/>
+        <ellipse cx="12.3303" cy="24.3809" rx="0.546102" ry="0.54633" fill="white"/>
+        <ellipse cx="17.6435" cy="6.27009" rx="0.546102" ry="0.54633" fill="white"/>
+        <ellipse cx="6.58028" cy="11.4603" rx="0.546102" ry="0.54633" fill="white"/>
+        <ellipse cx="24.5822" cy="18.6443" rx="0.546102" ry="0.54633" fill="white"/>
+        <defs>
+            <linearGradient id="paint0_linear_31_1738" x1="-13.8371" y1="6.32459" x2="28.7603" y2="15.1954" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0"/>
+                <stop offset="1" stop-color="white"/>
+            </linearGradient>
+            <linearGradient id="paint1_linear_31_1738" x1="2.13615" y1="17.1145" x2="46.6435" y2="19.298" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="paint2_linear_31_1738" x1="19.7482" y1="-11.1579" x2="16.0065" y2="29.402" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0"/>
+                <stop offset="1" stop-color="white"/>
+            </linearGradient>
+            <linearGradient id="paint3_linear_31_1738" x1="13.4205" y1="2.93142" x2="13.0147" y2="36.3723" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+            </linearGradient>
+        </defs>
+    </svg>
+)
