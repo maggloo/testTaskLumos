0c4dd292539236aff3d9027ce5cda78e813c53eb
Margarita P <rprivalko@gmail.com>
2024-09-10 22:41:40 +0300

    add fonts

---
+++ src/shared/fonts/roboto.ts
@@ -0,0 +1,7 @@
+import { Roboto } from 'next/font/google'
+export const roboto = Roboto({
+    display: 'swap',
+    subsets: ['latin'],
+    variable: '--font-roboto',
+    weight: ['400', '500', '700'],
+})
